3d32b41ae467f23236e0670087f066aa6c185d4a
MasterKelwin <mkelwin@outlook.com>
2022-04-12 22:15:16 -0300

    update enquanto tomo uma caipirinha

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -10,6 +11,8 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+    private  readonly SABADO = 6;
+    private readonly DOMINGO = 0;
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -19,15 +22,19 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
-        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) {
-            this.negociacoes.adiciona(negociacao); 
-            this.limpaFormulario();
-            this.atualizaView();
-        } else {
-            this.mensagemView.update('Apenas negociações em dias úteis são aceitas.')
-        }
+        const negociacao  = this.criaNegociacao();
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas.');
+            return;
+        }   
+        this.negociacoes.adiciona(negociacao); 
+        this.limpaFormulario();
+        this.atualizaView();
+    }
 
+    private ehDiaUtil (data: Date) {
+        return data.getDay() > DiasDaSemana.DOMINGO 
+        && data.getDay() < DiasDaSemana.SABADO;
     }
 
     private criaNegociacao(): Negociacao {
